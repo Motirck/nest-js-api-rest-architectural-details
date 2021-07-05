abb25ce49ea67c5e4c54a49c540f8116a2e45f18
Ricardo Alves Paula <ricardo.alves.p001@gmail.com>
2021-07-05 17:48:32 -0300

    Implemented global pipes validations

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Necessary 'useGlobalPipes' to define that API will be able to use Pipes for validations
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true
+  }));
+
   await app.listen(3000);
 }
 bootstrap();
--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,10 +1,30 @@
-
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 export class User {
     id: number;
+
+    @IsNotEmpty({
+        message: 'Username is required.'
+    })
+    @IsString({
+        message: 'Username must be a string.'
+    })
     userName: string;
+
+    @IsEmail({},{
+        message: 'Email must be a valid email address.'
+    })
     email: string;
+
+    @IsNotEmpty({
+        message: 'Password is required.'
+    })
     password: string;
+
+    @IsNotEmpty({
+        message: 'Full name is required.'
+    })
     fullName: string;
+
     createdAt: Date;
 }
