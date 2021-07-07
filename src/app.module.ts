74e5aaa9e93721b99787d395a99333a4da3ddff1
Ricardo Alves Paula <ricardo.alves.p001@gmail.com>
2021-07-07 16:09:42 -0300

    generic error handling implemented. Suitable for both Express and Fastify or any other.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { ClassSerializerInterceptor, Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { ExceptionHttp } from './common/filters/exception-http.filter';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [UserModule],
   controllers: [],
   providers: [
+    {
+      provide: APP_FILTER,
+      useClass: ExceptionHttp
+    },
     {
       provide: APP_INTERCEPTOR,
       useClass: ClassSerializerInterceptor
---
+++ src/common/filters/exception-http.filter.ts
@@ -0,0 +1,38 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from "@nestjs/common";
+import { AbstractHttpAdapter, HttpAdapterHost } from "@nestjs/core";
+import { throws } from "assert";
+
+@Catch()
+export class ExceptionHttp implements ExceptionFilter {
+
+    private httpAdapter: AbstractHttpAdapter;
+
+    constructor(adapterHost: HttpAdapterHost) {
+        this.httpAdapter = adapterHost.httpAdapter;
+    }
+
+    catch(exception: Error, host: ArgumentsHost) {
+        const context = host.switchToHttp();
+        const request = context.getRequest();
+        const response = context.getResponse();
+
+        const { status, body } = exception instanceof HttpException
+            ?
+            {
+                status: exception.getStatus(),
+                body: exception.getResponse()
+            }
+            :
+            {
+                status: HttpStatus.INTERNAL_SERVER_ERROR,
+                body: {
+                    statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+                    timestamp: new Date().toISOString(),
+                    message: exception.message,
+                    path: request.path
+                }
+            };
+
+        this.httpAdapter.reply(response, body, status);
+    }
+}
